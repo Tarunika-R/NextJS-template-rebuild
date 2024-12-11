085983fb49a7140d7b885e23f8b2bb0a8d0f4065
taru <taru253@gmail.com>
2024-12-11 20:40:54 +0530

    `Update dependencies and layout for Next.js app`

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,15 @@
+import { TfiEmail } from 'react-icons/tfi'; // Heroicons
+
+export default function Header() {
+  return (
+    <header style={{background:'#f9efe6'}} className='flex justify-between items-center px-4 py-5 text-#ef7a54'>
+      <div>
+        <img src="/assets/images/logo.png" alt='logo' className='h-[5.1rem]'/>
+      </div>
+      <button style={{color:'#ef7a54'}} className="message-icon flex items-center pr-4">
+        <TfiEmail className='text-3xl rounded-xl'/>
+        <span style={{color:'#ef7a54'}} className='ml-2 text-xl font-sans font-normal'>abc@zealtaste.com</span>
+      </button>
+    </header>
+  )
+}
---
+++ src/app/components/MainSection1.jsx
@@ -0,0 +1,9 @@
+const MainSection1 = () => {
+  return (
+    <div className="w-full flex justify-center md:justify-end md:pt-9 md:pr-12">
+      <img src="/assets/images/image1.png" alt="main-section-1" className="h-[65rex] w-[50rex] pt-16 rounded-t-full"/>
+    </div>
+  )
+}
+
+export default MainSection1
